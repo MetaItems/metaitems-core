03f9b692e8ea306a678296ba89ec330c6910b0bb
The Collector <contact@minte.tv>
2022-02-06 16:28:57 -0400

    fix: registration page, pass input fix

--- lib/metaitems/accounts/user.ex
+++ lib/metaitems/accounts/user.ex
@@ -83,10 +83,10 @@ defmodule Metaitems.Accounts.User do
     if register_user? do
       changeset
       |> validate_required([:password])
-      |> validate_length(:password, min: 6, max: 72)
-      # |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
-      # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
-      # |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
+      |> validate_length(:password, min: 8, max: 72)
+      |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
+      |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
+      |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
       |> maybe_hash_password(opts)
     else
       changeset
