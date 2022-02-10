64b5ffbe4f8983ec8d55d6a97eb2cf0c56317049
The Collector <contact@minte.tv>
2022-02-09 20:24:41 -0400

    fix: release file

--- lib/metaitems/release.ex
+++ lib/metaitems/release.ex
@@ -6,7 +6,7 @@ defmodule Metaitems.Release do
   @app :metaitems
 
   def migrate do
-    load_app()
+    start_app()
 
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
@@ -14,7 +14,7 @@ defmodule Metaitems.Release do
   end
 
   def rollback(repo, version) do
-    load_app()
+    start_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
@@ -27,8 +27,11 @@ defmodule Metaitems.Release do
   end
 
   defp start_app do
+    Application.ensure_all_started(:ssl)
+
     load_app()
     Application.put_env(@app, :minimal, true)
-    Application.ensure_all_started(@app)
+    # Application.ensure_all_started(@app)
+
   end
 end
